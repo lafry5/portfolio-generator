9a3a8ad877b58beef697d61aa5603028d6c218e7
lafry5 <lafry5@yahoo.com>
2020-07-22 08:12:13 -0700

    added gitignore and first user input

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-const fs = require('fs');
 
-const generatePage = require('./src/page-template.js');  // required to link to function in other modules
+// FORMER CODE
+// const fs = require('fs');
 
+// const generatePage = require('./src/page-template.js');  // required to link to function in other modules
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const pageHTML = generatePage(portfolioData); This wasn't in the code before
 
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
 // or const [name, github] = profileDataArgs;
 
 
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+//   fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
+
+const inquirer = require('inquirer');
+// console.log(inquirer)
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
